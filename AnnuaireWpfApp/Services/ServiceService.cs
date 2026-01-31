251927756bf1c9759218f3fd7c29794d888250b6
Porygon5 <VALLIERMATHIS.PRO@GMAIL.COM>
2026-01-31 15:38:57 +0100

    feat : Ajout du service SiteService et ServiceService pour la gestion des sites et services

---
+++ AnnuaireWpfApp/Services/ServiceService.cs
@@ -0,0 +1,61 @@
+﻿using AnnuaireModel.Context;
+using AnnuaireModel.Entities;
+
+namespace AnnuaireWpfApp.Services
+{
+    public class ServiceService
+    {
+        private readonly AnnuaireContext _context;
+
+        public ServiceService(AnnuaireContext context)
+        {
+            _context = context;
+        }
+
+        // récupérer les services
+        public List<Service> GetAll()
+        {
+            return _context.Services.ToList();
+        }
+
+        // getbyid
+        public Service? GetById(int id)
+        {
+            return _context.Services.Find(id);
+        }
+
+        // ajouter un service
+        public void Add(Service service)
+        {
+            _context.Services.Add(service);
+            _context.SaveChanges();
+        }
+
+        //update 
+        public void Update(Service service)
+        {
+            _context.Services.Update(service);
+            _context.SaveChanges();
+        }
+
+        // supprimer un service
+        public bool Delete(int id)
+        {
+            var service = _context.Services.Find(id);
+            if (service == null) return false;
+
+            // on vérifie bien que le service n'ait pas d'employés
+            bool hasEmployes = _context.Employes.Any(e => e.ServiceId == id);
+
+            if (hasEmployes)
+            {
+                throw new Exception("Impossible de supprimer : des employés sont affectés à ce service");
+            }
+
+            _context.Services.Remove(service);
+            _context.SaveChanges();
+
+            return true;
+        }
+    }
+}
---
+++ AnnuaireWpfApp/Services/SiteService.cs
@@ -0,0 +1,61 @@
+﻿using AnnuaireModel.Context;
+using AnnuaireModel.Entities;
+
+namespace AnnuaireWpfApp.Services
+{
+    public class SiteService
+    {
+        private readonly AnnuaireContext _context;
+
+        public SiteService(AnnuaireContext context)
+        {
+            _context = context;
+        }
+
+        // récupérer les sites
+        public List<Site> GetAll()
+        {
+            return _context.Sites.ToList();
+        }
+
+        // getbyid
+        public Site? GetById(int id)
+        {
+            return _context.Sites.Find(id);
+        }
+
+        // ajouter un site
+        public void Add(Site site)
+        {
+            _context.Sites.Add(site);
+            _context.SaveChanges();
+        }
+
+        //update 
+        public void Update(Site site)
+        {
+            _context.Sites.Update(site);
+            _context.SaveChanges();
+        }
+
+        // supprimer un site
+        public bool Delete(int id)
+        {
+            var site = _context.Sites.Find(id);
+            if (site == null) return false;
+
+            // on vérifie bien que le site n'ait pas d'employés
+            bool hasEmployes = _context.Employes.Any(e => e.SiteId == id);
+
+            if (hasEmployes)
+            {
+                throw new Exception("Impossible de supprimer : des employés sont affectés à ce site");
+            }
+
+            _context.Sites.Remove(site);
+            _context.SaveChanges();
+
+            return true;
+        }
+    }
+}
